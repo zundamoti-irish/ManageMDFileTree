0a88ab0fe2de9019e0b2438d29f187c71f3e6756
zundamoti <79198550+zundamoti-irish@users.noreply.github.com>
2023-11-28 22:55:04 +0900

    ソースコード追加
    MarkDown (PlantUML)形式で、ファイル構造を出力できるメインソースコードを追加

---
+++ ManageMDFileTree.py
@@ -0,0 +1,34 @@
+#pathlibは別途pipでインストールしておく。
+import pathlib
+import os
+
+def main():
+    Folderpath = os.getcwd()#対象フォルダを指定
+    outputpath = 'index.md'#テキストファイルを作成
+    f = open(outputpath, mode='w')#テキストファイルを書き込みモードで開く
+
+    output = '```plantuml\n@startuml\ntitle ディレクトリ構成図\nskinparam TitleFontSize 0\n\nsalt\n\n{\n\t{T\n'
+    f.write(output)
+    #実行ファイルのパスを追加
+    subdirname = os.path.basename(str(Folderpath))
+    output = '+ ' + str(subdirname) + '\t| [概要を記載]' + '\n'
+    
+    f.write(output)
+
+    GetFolderFileNames(Folderpath, 0, f)
+    output = '\t}\n}\n@enduml\n```'
+    f.write(output)
+
+
+def GetFolderFileNames(path, kaiso, f):
+    files = pathlib.Path(path).glob('*')#その階層のフォルダやファイルを取得
+
+    for file in files:
+        output = '\t' * (kaiso + 1) + '+' * (kaiso + 2) + ' ' + file.name + '\t| [概要を記載]' + '\n'
+        f.write(output)
+
+        if file.is_file() == False:
+            GetFolderFileNames(file, kaiso+1, f)
+
+if __name__ == "__main__":
+    main()
